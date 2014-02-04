0f06af940922fd92699fc28d82f430226f81d60c
sameera207 <sameera207@gmail.com>
2014-02-04 23:20:33 +1100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shopper::Application.config.secret_key_base = '965d9494d7243e9201ea7143fe110dbd85dae13ddf0ca1110ffa7806f0c457cd54151fd5c59ee97b2e0703983023cde0be3263cf8b3593a26094ada49fadb89b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Shopper::Application.config.session_store :cookie_store, key: '_shopper_session'
